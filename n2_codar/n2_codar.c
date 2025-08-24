48ce38649e7cbd860f5e96776ef6daa5905a07ab
maxlanionunes <max.lanio.12@gmail.com>
2025-08-24 15:39:56 -0300

    fim do exercicio

--- n2_codar/n2_codar.c
+++ n2_codar/n2_codar.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
+    int idade, matricula; 
+    float altura;
+    char nome[50];
 
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+    //fgets(nome, 50, stdin);
     
+    printf("Digite sua matricula: ");
+    scanf("%d", &matricula);
+    
+
+    printf("\n");
+
+    printf("Nome do aluno: %s - Matricula: %d \n", nome, matricula);
+    printf("Idade: %d - altura: %f", idade, altura);
+
+    return 0;
 }
